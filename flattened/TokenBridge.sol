adf51a95341688faea234a5f5baf2cce74242f40
naws-dev <john@naws.ai>
2025-10-17 22:11:29 +0900

    fix flatten logic

--- flattened/TokenBridge.sol
+++ flattened/TokenBridge.sol
@@ -1,15 +1,96 @@
-// SPDX-License-Identifier: MIT
-// Flattened TokenBridge contract
-// This file contains all dependencies in a single file for easy deployment
+// Sources flattened with hardhat v2.23.0 https://hardhat.org
 
-// ===== IERC20Metadata Interface =====
 // SPDX-License-Identifier: MIT
-// OpenZeppelin Contracts (last updated v5.0.0) (token/ERC20/extensions/IERC20Metadata.sol)
+
+// File contracts/openzeppelin-contracts-5.0.0/token/ERC20/IERC20.sol
+
+// Original license: SPDX_License_Identifier: MIT
+// OpenZeppelin Contracts (last updated v5.0.0) (token/ERC20/IERC20.sol)
 
 pragma solidity ^0.8.20;
 
-import {IERC20} from "../IERC20.sol";
+/**
+ * @dev Interface of the ERC20 standard as defined in the EIP.
+ */
+interface IERC20 {
+    /**
+     * @dev Emitted when `value` tokens are moved from one account (`from`) to
+     * another (`to`).
+     *
+     * Note that `value` may be zero.
+     */
+    event Transfer(address indexed from, address indexed to, uint256 value);
 
+    /**
+     * @dev Emitted when the allowance of a `spender` for an `owner` is set by
+     * a call to {approve}. `value` is the new allowance.
+     */
+    event Approval(address indexed owner, address indexed spender, uint256 value);
+
+    /**
+     * @dev Returns the value of tokens in existence.
+     */
+    function totalSupply() external view returns (uint256);
+
+    /**
+     * @dev Returns the value of tokens owned by `account`.
+     */
+    function balanceOf(address account) external view returns (uint256);
+
+    /**
+     * @dev Moves a `value` amount of tokens from the caller's account to `to`.
+     *
+     * Returns a boolean value indicating whether the operation succeeded.
+     *
+     * Emits a {Transfer} event.
+     */
+    function transfer(address to, uint256 value) external returns (bool);
+
+    /**
+     * @dev Returns the remaining number of tokens that `spender` will be
+     * allowed to spend on behalf of `owner` through {transferFrom}. This is
+     * zero by default.
+     *
+     * This value changes when {approve} or {transferFrom} are called.
+     */
+    function allowance(address owner, address spender) external view returns (uint256);
+
+    /**
+     * @dev Sets a `value` amount of tokens as the allowance of `spender` over the
+     * caller's tokens.
+     *
+     * Returns a boolean value indicating whether the operation succeeded.
+     *
+     * IMPORTANT: Beware that changing an allowance with this method brings the risk
+     * that someone may use both the old and the new allowance by unfortunate
+     * transaction ordering. One possible solution to mitigate this race
+     * condition is to first reduce the spender's allowance to 0 and set the
+     * desired value afterwards:
+     * https://github.com/ethereum/EIPs/issues/20#issuecomment-263524729
+     *
+     * Emits an {Approval} event.
+     */
+    function approve(address spender, uint256 value) external returns (bool);
+
+    /**
+     * @dev Moves a `value` amount of tokens from `from` to `to` using the
+     * allowance mechanism. `value` is then deducted from the caller's
+     * allowance.
+     *
+     * Returns a boolean value indicating whether the operation succeeded.
+     *
+     * Emits a {Transfer} event.
+     */
+    function transferFrom(address from, address to, uint256 value) external returns (bool);
+}
+
+
+// File contracts/openzeppelin-contracts-5.0.0/token/ERC20/extensions/IERC20Metadata.sol
+
+// Original license: SPDX_License_Identifier: MIT
+// OpenZeppelin Contracts (last updated v5.0.0) (token/ERC20/extensions/IERC20Metadata.sol)
+
+pragma solidity ^0.8.20;
 /**
  * @dev Interface for the optional metadata functions from the ERC20 standard.
  */
@@ -31,8 +112,9 @@ interface IERC20Metadata is IERC20 {
 }
 
 
-// ===== ReentrancyGuard =====
-// SPDX-License-Identifier: MIT
+// File contracts/openzeppelin-contracts-5.0.0/utils/ReentrancyGuard.sol
+
+// Original license: SPDX_License_Identifier: MIT
 // OpenZeppelin Contracts (last updated v5.0.0) (utils/ReentrancyGuard.sol)
 
 pragma solidity ^0.8.20;
@@ -118,13 +200,10 @@ abstract contract ReentrancyGuard {
 }
 
 
-// ===== TokenBridge Contract =====
-// SPDX-License-Identifier: MIT
+// File contracts/TokenBridge.sol
+
+// Original license: SPDX_License_Identifier: MIT
 pragma solidity 0.8.20;
-
-import {IERC20Metadata} from "./openzeppelin-contracts-5.0.0/token/ERC20/extensions/IERC20Metadata.sol";
-import {ReentrancyGuard} from "./openzeppelin-contracts-5.0.0/utils/ReentrancyGuard.sol";
-
 contract TokenBridge is ReentrancyGuard {
     IERC20Metadata public immutable sourceToken;
     
@@ -169,4 +248,4 @@ contract TokenBridge is ReentrancyGuard {
         
         emit Bridged(msg.sender, receiverBEP20Address, amount);
     }
-} 
+}
